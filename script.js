b3cc966e6cfffe924bae0af1b26853b6c3df7c82
jayakrishnapalla05 <130897074+jayakrishnapalla05@users.noreply.github.com>
2023-06-21 14:04:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-//your JS code here. If required.
-var c = document.getElementById("level");
-var level = 0;
-while (c.parentNode) {
-  level++;
-  c = c.parentNode;
+// //your JS code here. If required.
+// var c = document.getElementById("level");
+// var level = 0;
+// while (c.parentNode) {
+//   level++;
+//   c = c.parentNode;
+// }
+// var message = "The level of the element is: " + level;
+// alert(message);
+let level = document.querySelectorAll("ul li")
+let domLevel = 0;
+
+for (let i = 0; i < level.length; i++) {
+  if (level[i].id === "level") {
+    domLevel = i +1;
+    break;
+  }
 }
-var message = "The level of the element is: " + level;
-alert(message);
+alert("The level of the element is: " + domLevel);
